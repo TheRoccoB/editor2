7b859b6eceef69362ccac11637f5e3fee36f8912
Rocco Balsamo <rocco@tintup.com>
2016-04-29 11:45:40 -0700

    accordion

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(function() {
-    console.log('javascript is loaded');
+    var accordion = $('#yourDiv').accordionPro({
+        "horizontalWidth":1200,
+        "horizontalHeight":500,
+        responsive:true,
+        theme:'stitch',
+        colour : {
+            scheme : 'orange',
+        },
+
+    });
+
+    console.log(accordion);
+
+
 });
