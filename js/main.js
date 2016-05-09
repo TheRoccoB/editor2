e7756f233fc7b40ea0831e78baede51840696b8d
Rocco Balsamo <rocco@tintup.com>
2016-05-09 15:03:36 -0700

    update

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@ $(function() {
         responsive:true,
         theme:'stitch',
         colour : {
-            scheme : 'orange',
-        },
-
+            scheme : 'orange'
+        }
     });
 
-    console.log(accordion);
+    $('.contentType').click(function(){
+        $('#chooseTheme1').hide(1000);
+        $('#chooseTheme2').show(1000);
+    });
 
+    $('.themeType #next1').click(function(){
 
+        $('#yourDiv').accordionPro('trigger', 2)
+
+    });
 });
